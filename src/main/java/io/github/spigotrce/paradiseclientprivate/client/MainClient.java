ac957dae70922cf373e726d38972a78fa66582ac
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-30 14:12:00 +0530

    Init

---
+++ src/main/java/io/github/spigotrce/paradiseclientprivate/client/MainClient.java
@@ -0,0 +1,10 @@
+package io.github.spigotrce.paradiseclientprivate.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
