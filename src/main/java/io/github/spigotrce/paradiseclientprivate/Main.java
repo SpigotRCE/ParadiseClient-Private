915671d425b86e53e74ee39885cce65cf2535d85
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-31 11:13:11 +0530

    registered authme exploit command

--- src/main/java/io/github/spigotrce/paradiseclientprivate/Main.java
+++ src/main/java/io/github/spigotrce/paradiseclientprivate/Main.java
@@ -2,12 +2,19 @@ package io.github.spigotrce.paradiseclientprivate;
 
 import io.github.spigotrce.paradiseclientfabric.Constants;
 import io.github.spigotrce.paradiseclientfabric.ParadiseClient_Fabric;
+import io.github.spigotrce.paradiseclientprivate.command.AuthMeBypassCommand;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
 
 public class Main implements ModInitializer {
 
     @Override
     public void onInitialize() {
         Constants.EDITION = "PRIVATE";
+        registerCommands();
+    }
+
+    private void registerCommands() {
+        ParadiseClient_Fabric.getCommandManager().register(new AuthMeBypassCommand(MinecraftClient.getInstance()));
     }
 }
